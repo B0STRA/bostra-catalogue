5d96d30f2fb0eaf06f5a38cee7c21bf03ce1acf3
B0STRA <119994243+B0STRA@users.noreply.github.com>
2023-08-31 14:23:27 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,9 @@
 fx_version "cerulean"
 
 description "Basic React (TypeScript) & Lua Game Scripts Boilerplate"
-author "Project Error"
+author "Bostra"
 version '1.0.0'
-repository 'https://github.com/project-error/fivem-react-boilerplate-lua'
+repository 'https://github.com/b0stra'
 
 lua54 'yes'
 
@@ -14,8 +14,13 @@ games {
 
 ui_page 'web/build/index.html'
 
-client_script "client/**/*"
-server_script "server/**/*"
+client_scripts { "client/**/*",
+"@ox_lib/init.lua"
+}
+server_scrips {
+  "server/**/*",
+  "@ox_lib/init.lua"
+}
 
 files {
 	'web/build/index.html',
